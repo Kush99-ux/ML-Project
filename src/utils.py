337f6571e2dac82edbb69f11ca942cdb2f912dd3
Kush Sahu <kushs0985@gmail.com>
2025-12-27 11:24:34 +0530

    Hyper Parameter tuning

--- src/utils.py
+++ src/utils.py
@@ -1,39 +1,56 @@
 import os
 import sys
-
-import numpy as np
-import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
 
-def save_object(file_path,obj):
+def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
 
-        os.makedirs(dir_path,exist_ok = True)
-
-        with open(file_path,"wb") as file_obj:
-            dill.dump(obj,file_obj)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e,sys)
-    
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    """
+    Evaluate multiple models using GridSearchCV (Python 3 compatible)
+    """
     try:
         report = {}
 
         for model_name, model in models.items():
+            params = param.get(model_name, {})
+
+            # If params exist → GridSearch
+            if params:
+                gs = GridSearchCV(
+                    model,
+                    params,
+                    cv=3,
+                    n_jobs=-1
+                )
+                gs.fit(X_train, y_train)
+
+                model.set_params(**gs.best_params_)
+
+            # Train final model
             model.fit(X_train, y_train)
 
+            # Predict & score
             y_test_pred = model.predict(X_test)
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[model_name] = test_model_score
 
         return report
-    
+
     except Exception as e:
-        raise  CustomException(e,sys)
+        raise CustomException(e, sys)
